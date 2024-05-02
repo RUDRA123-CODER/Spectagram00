018377cb348a72708ec7d26151d5c970e1fa4785
RUDRA123-CODER <78489800+RUDRA123-CODER@users.noreply.github.com>
2024-05-02 23:35:47 +0530

    Add files via upload

---
+++ spectagram/screens/Createpost.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import { Text, View} from "react-native";
+
+export  default class CreatePost extends Component{
+    render(){
+        <view
+        style={{
+            flex:1,
+            justifyContent:"center",
+            alignItems:"center"
+        }}>
+        <Text>Create Post</Text>    
+        </view>
+    }
+}
---
+++ spectagram/screens/Profile.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import {Text,View} from "react-native";
+export  default class CreatePost extends Component{
+    render(){
+        <View
+        style={{
+            flex:1,
+            justifyContent:"center",
+            alignItems:"center"
+        }}>
+        <Text>Create Post</Text>    
+        </View>
+    }
+}
